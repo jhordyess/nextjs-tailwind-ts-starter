7d362bc4d3415f0284c640758e445a87702d4f13
jhordyess <dev@jhordyess.com>
2025-04-24 12:24:27 -0400

    Add global styles and layout components for Next.js application

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import './globals.css'
+
+const roboto = Roboto({
+  variable: '--font-roboto',
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '500', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Nextjs Tailwind CSS and Typescript Starter'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  )
+}
